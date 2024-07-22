8b2f81d1e8e26e30c159a64fd293ebd34fa75302
andrian <andriantodwilaksono100104@gmail.com>
2024-07-22 15:49:18 +0700

    push changes

--- app/dashboard/product/page.tsx
+++ app/dashboard/product/page.tsx
@@ -4,14 +4,14 @@ import Link from "next/link"
 import Image from "next/image"
 import kaos1 from "@/public/ratell.black.png"
 import kaos2 from "@/public/ratell.white.png"
+import ukuran from "@/public/ukuran.jpg"
 
 export default function Page() {
     const telp = process.env.NEXT_PUBLIC_TELP
     const pesan =`halo saya ingi membeli kaos`
-
     const link = `https://wa.me/${telp}?text=${pesan}.`
     return (
-    <div className="inline-grid grid-cols-2 gap-9 ">
+    <div className="inline-grid display-flex gap-2 ">
     <div className=" px-3 w-90 bg-white border border-gray-800 rounded-lg shadow dark:bg-gray-200 dark:border-gray-700">
         <a href="#">
             <Image 
@@ -29,11 +29,11 @@ export default function Page() {
             </a>
             <div className="flex items-center mt-2.5 mb-5">
                 <div className="flex items-center space-x-1 rtl:space-x-reverse">
-                    Black 
+                    Burn The Cops - Black 
                 </div>
             </div>
-            <div className="flex items-center justify-between space-x-1">
-                <span className="text-3xl font-bold text-gray-900 ">150.000</span>
+            <div className="flex items-center justify-between space-x-1"> Harga :
+                <span className=" text-base font-bold text-gray-900 ">150.000</span>
                 
             </div>
             
@@ -53,23 +53,23 @@ export default function Page() {
             <a href="#">
                 <h5 className="text-xl font-semibold tracking-tight text-gray-900">T-Shirt 1312 </h5>
             </a>
-            <div className="flex items-center mt-2.5 mb-5">
+        <div className="flex items-center mt-2.5 mb-5">
                 <div className="flex items-center space-x-1 rtl:space-x-reverse">
-                    White
+                    Burn The Cops - White
                 </div>
+         </div>
+            <div className="flex items-center justify-between"> Harga :
+                <span className=" font-bold text-gray-900 text-base ">150.000</span>
             </div>
-            <div className="flex items-center justify-between">
-                <span className="text-3xl font-bold text-gray-900 ">150.000</span>
-            </div>
-            
         </div>
      </div>
-                 <div className="flex flex-auto justify-center ">
+             <div className="px-8 justify-center">
                 <Link href={link} target="_blank">
                     <button  className="text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-9 py-5 text-center dark:bg-gray-800 dark:hover:bg-gray-500">Klik disini untuk pesan</button>
                 </Link>
-                </div>
+            </div>
      </div>
+     
     
     )
   }
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -26,12 +26,6 @@ export default function SideNav() {
       <div className="flex grow flex-row justify-between  space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-white md:block "></div>
-        <form>
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-red-100 hover:text-red-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <PowerIcon className="w-6" />
-            <div className="hidden md:block">Sign Out</div>
-          </button>
-        </form>
       </div>
     </div>
   );
