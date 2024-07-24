98605e12f0108d88369ffe7514dd7c5a57352817
andrian <andriantodwilaksono100104@gmail.com>
2024-07-24 19:54:36 +0700

    push changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,38 +5,44 @@ import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 import logo from '@/public/logo.png'
+import Head from 'next/head';
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6 ">
+    <>
+     <Head>
+      <title>Ratell.1312</title>
+      <link rel="icon" href='logo.png' />
+     </Head>
+      <main className="flex min-h-screen flex-col p-6 ">
       <div className="mt-4 flex grow flex-col gap-3 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-        <div>
+          <div>
             <a className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-              <strong>Welcome to ratell1312.com</strong>
-           <p>
-            our official website
-           </p>
-          </a>
+              <strong>Welcome to Ratell.1312</strong>
+              <p>
+                Our Official Website
+              </p>
+            </a>
           </div>
-          <div/>
+          <div />
           <Link
-            href="/dashboard" 
+            href="/dashboard"
             className="flex items-center gap-5 self-start rounded-lg bg-gray-200 px-6 py-3 text-sm font-medium text-black transition-colors hover:bg-gray-900 hover:text-white md:text-base"
           >
             <span>Masuk</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-         <Image 
-         className='rounded-lg'
-          src={logo}
-          alt='logo'
-          height={0}
-          width={0}
-         />
+          <Image
+            className='rounded-lg'
+            src={logo}
+            alt='logo'
+            height={0}
+            width={0} />
         </div>
       </div>
     </main>
+    </>
   );
 }
