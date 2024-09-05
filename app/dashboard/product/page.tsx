ba95855156342108021a8e9accd8de4fc5fc59dc
andrian <andriantodwilaksono100104@gmail.com>
2024-09-05 15:33:12 +0700

    push

--- app/dashboard/product/page.tsx
+++ app/dashboard/product/page.tsx
@@ -1,137 +1,70 @@
 'use client'
+import ratell1 from "@/public/ratell.white.png"
+import ratell2 from "@/public/ratell.black.png"
 
-import Link from "next/link"
-import Image from "next/image"
-import kaos1 from "@/public/ratell.black.png"
-import kaos2 from "@/public/ratell.white.png"
-import ukuran from "@/public/ukuran.jpg"
-import { useEffect, useState } from "react"
-import { StaticImageData } from "next/image"
-import { ChevronLeft, ChevronRight } from "lucide-react";
 
 // Interface untuk image data
-interface ImageData {
-    src: StaticImageData;
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/aspect-ratio'),
+    ],
   }
-  
-  // Image data array
-  const images: ImageData[] = [
-    {
-      src: kaos1,
-    },
-    {
-      src: kaos2,
-    },
-    {
-      src: ukuran,
-    },
-  ];
-  
-  export default function ImageSlider(): JSX.Element {
-    // Suntuk useState
-    const [currentIndex, setCurrentIndex] = useState<number>(0);
-  
-    // untuk hover
-    const [isHovered, setIsHovered] = useState<boolean>(false);
-    //untuk pesan ke whatsapp
-    const telp = process.env.NEXT_PUBLIC_TELP
-    const pesan =`halo saya ingin membeli kaos
-    warna:
-    size:
-    jumlah:
-    `
-    const link = `https://wa.me/${telp}?text=${pesan}.`
-  
-    // Function untuk menampilkan tampilan sebelumnya
-    const prevSlide = (): void => {
-      setCurrentIndex(
-        (prevIndex) => (prevIndex - 1 + images.length) % images.length
-      );
-    };
-  
-    // Function to show the next slide
-    const nextSlide = (): void => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    };
-  
-    // useEffect hook to handle automatic slide transition
-    useEffect(() => {
-      // Start interval for automatic slide change if not hovered
-      if (!isHovered) {
-        const interval = setInterval(() => {
-          ;
-        });
-  
-        // Cleanup the interval on component unmount
-        return () => {
-          clearInterval(interval);
-        };
-      }
-    }, [isHovered]);
-  
-    // Handle mouse over event
-    const handleMouseOver = (): void => {
-      setIsHovered(true);
-    };
-  
-    // Handle mouse leave event
-    const handleMouseLeave = (): void => {
-      setIsHovered(false);
-    };
-  
-    return (
-      <div className="relative w-full mx-auto mt-4">
-        <div
-          className="relative h-[460px] mx-12 group hover:-translate-y-2"
-          onMouseOver={handleMouseOver}
-          onMouseLeave={handleMouseLeave}
-        >
-          <Image
-            src={images[currentIndex].src}
-            alt={`Slider Image ${currentIndex + 1}`}
-            layout="fill"
-            objectFit="contain"
-            className="rounded-xl transition-all ease-in-out"
-          />
+  ```
+*/
+const callouts = [
+  {
+    name: 'Black Variant',
+    description: 'Burn The Cops',
+    imageSrc: "https://ratell1312.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fratell.white.87ca3d3e.png&w=1920&q=75",
+    imageAlt: '',
+    href: '#',
+  },
+  {
+    name: 'White Variant',
+    description: 'Burn The Cops',
+    imageSrc: 'https://ratell1312.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fratell.black.13f14c29.png&w=1920&q=75',
+    imageAlt: '',
+    href: '#',
+  },
+]
+
+export default function Example() {
+  return (
+    <div className="bg-gray-100">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
+          <h2 className="text-2xl font-bold text-gray-900">Collections</h2>
+
+          <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+            {callouts.map((callout) => (
+              <div key={callout.name} className="group relative">
+                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                  <img
+                    alt={callout.imageAlt}
+                    src={callout.imageSrc}
+
+                    className="h-full w-full object-cover object-center"
+                  />
+                </div>
+                <h3 className="mt-6 text-sm text-gray-500">
+                  <a href={callout.href}>
+                    <span className="absolute inset-0" />
+                    {callout.name}
+                  </a>
+                </h3>
+                <p className="text-base font-semibold text-gray-900">{callout.description}</p>
+              </div>
+            ))}
+          </div>
         </div>
-        <button
-          className="absolute left-0 top-1/2 transform h-[459px] rounded-xl mx-1 -mt-[10px] -translate-y-1/2 bg-white text-white p-2 group"
-          onClick={prevSlide}
-        >
-          <ChevronLeft className="text-black group-hover:text-gray-700" />
-        </button>
-        <button
-          className="absolute right-0 top-1/2 transform h-[459px] rounded-xl mx-1 -mt-[10px] -translate-y-1/2 bg-white text-white p-2 group"
-          onClick={nextSlide}
-        >
-          <ChevronRight className="text-black group-hover:text-gray-700" />
-        </button>
-        <div className="flex justify-center mt-4">
-          {images.map((_, index) => (
-            <div
-              key={index}
-              className={`h-1 w-10 mx-1 ${
-                index === currentIndex
-                  ? "bg-gray-800 rounded-xl"
-                  : "bg-gray-300 rounded-xl"
-              } transition-all duration-500 ease-in-out`}
-            ></div>
-          ))}
-        </div>
-        <div>
-            <div className="flex justify-center">
-            <a className="px-11 justify-center">
-                deskripsi:
-                kaos 24s, di atas XL +5ribu, lengan panjang +10ribu.Po 80ribu.
-                Harga: Rp 150.000
-            </a>
-            </div>
-        </div>
-        <div className="flex justify-center mt-4">
-                <Link href={link} target="_blank">
-                    <button  className="text-black hover:text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-9 py-5 text-center dark:bg-gray-300 dark:hover:bg-gray-800">Klik disini untuk pesan</button>
-                </Link>
       </div>
-     </div>
-    );
-  }
+    </div>
+  )
+}
